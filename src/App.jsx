cac99090d0c91cb843a92c340801147b7a8b0292
Vinicius Vitório <vini.vtj@gmail.com>
2024-12-02 21:50:58 -0300

    Adicionado desenvolvedores

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import CurrencyConverterCard from "./components/CurrencyConverterCard";
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-950 p-6">
+    <div className="min-h-screen gap-4 flex flex-col items-center justify-center bg-gray-900 p-6">
       <CurrencyConverterCard />
+      <span className="text-gray-400 flex gap-2">Desenvolvido por <p className="text-blue-500">Vinicius Vitório</p> & <p className="text-blue-500">Pablo Pscheidt</p></span>
     </div>
   );
 }
